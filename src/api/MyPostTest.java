1f56b435ce7b80ab41e4702eccb9a21afc2c09ad
AyubQA <ayub@daguzhiev.ru>
2024-03-17 21:27:18 +0300

    Fix

---
+++ src/api/MyPostTest.java
@@ -0,0 +1,121 @@
+package api;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static api.data.JsonData.*;
+import static api.data.URLs.*;
+import static io.restassured.RestAssured.given;
+
+
+public class MyPostTest {
+
+    @Test
+    public void testRegistration() {
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(ContentType.JSON)
+                .body(BodyForRegistration)
+                .post(register)
+                .then()
+                .assertThat()
+                .log().status()
+                .log().body()
+                .statusCode(201);
+    }
+
+    @Test
+    public void TestLogin() {
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(ContentType.JSON)
+                .body(TestLogin)
+                .post(login)
+                .then()
+                .assertThat()
+                .log().status()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Get all product")
+    public void TestGetAllProducts() {
+        given()
+                .log().uri()
+                .when()
+                .get(products)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Add new product")
+    public void TestAddNewProduct() {
+        given()
+                .log().uri()
+                .log().body()
+                .body(BodyForAddProduct)
+                .contentType(ContentType.JSON)
+                .when()
+                .post(products)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Get information about specific product")
+    public void TestGetInfoAboutProduct() {
+        given()
+                .log().uri()
+                .log().body()
+                .queryParam("Product_Id", 1)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(products)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Update information about specific product")
+    public void TestUpdateInfoAboutProduct() {
+        given()
+                .log().uri()
+                .log().body()
+                .body(BodyForUpdateProduct)
+                .queryParam("Product_Id", 1)
+                .contentType(ContentType.JSON)
+                .when()
+                .put(products)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Delete information about specific product")
+    public void TestDeleteInfoAboutProduct() {
+        given()
+                .log().uri()
+                .log().body()
+                .queryParam("Product_Id", 8)
+                .contentType(ContentType.JSON)
+                .when()
+                .delete(products)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200);
+    }
+}
---
+++ src/api/data/URLs.java
@@ -0,0 +1,8 @@
+package api.data;
+
+public class URLs {
+    public static String register = "http://9b142cdd34e.vps.myjino.ru:49268/register";
+    public static String login = "http://9b142cdd34e.vps.myjino.ru:49268/login";
+    public static String products = "http://9b142cdd34e.vps.myjino.ru:49268/products";
+
+}
