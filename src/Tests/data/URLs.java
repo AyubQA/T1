75823e666e3cdd9f5f8b5d7671a2eea414750a55
AyubQA <ayub@daguzhiev.ru>
2024-03-17 21:11:22 +0300

    Fix

--- src/Tests/data/URLs.java
+++
@@ -1,8 +0,0 @@
-package Tests.data;
-
-public class URLs {
-    public static String register = "http://9b142cdd34e.vps.myjino.ru:49268/register";
-    public static String login = "http://9b142cdd34e.vps.myjino.ru:49268/login";
-    public static String products = "http://9b142cdd34e.vps.myjino.ru:49268/products";
-
-}
